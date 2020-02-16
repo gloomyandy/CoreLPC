3529c5e29e9c5ef93a92f6ca016ee9d006f39616
Andy Shaw <andy-git@gloomy-place.com>
2020-02-16 14:35:10 +0000

    Modified us ticker to use RIT Timer
    These changes switch the uS timer code from using TIMER3 to using th RIT
    timer. This frees up TIMER3 to allow the soft UART code to run at the
    same time as the ADC filter.
    
    Unfortunately the RIT timer does not have a pre-scaler option which
    measn that we have to run the RIT timer at full PCLK rather than scaling
    it to be 1 tick per uS. To preserve the external API we post scale the
    tick values, however this means that the longest delta that can be set
    is reduced form 2125 seconds to 85 seconds. This is not an issue with
    the way that this code is used at present (for software PWM generation),
    but should be noted if the code is used for other purposes.

--- CoreLPC2/cores/mbed/us_ticker_api.c
+++ CoreLPC2/cores/mbed/us_ticker_api.c
@@ -15,6 +15,7 @@
  */
 
 //SD:: Modified for use with RRF
+//GA:: Modified to use RIT timer and to avoid race condition
 #include "Core.h"
 #include "chip.h"
 #include <stddef.h>
@@ -25,38 +26,35 @@
        * "us_ticker_init", "us_ticker_read"
        * "us_ticker_set_interrupt", "us_ticker_disable_interrupt", "us_ticker_clear_interrupt"
  */
-#define US_TICKER_TIMER      ((LPC_TIMER_T *)LPC_TIMER3_BASE)
-#define US_TICKER_TIMER_IRQn TIMER3_IRQn
+#define US_TICKER_TIMER      ((LPC_RITIMER_T *)LPC_RITIMER_BASE)
 
 static int us_ticker_running = 0;
+static uint32_t postscale;
 
 static inline void us_ticker_init(void) {
     us_ticker_running = 1;
 
-    Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_TIMER3); //enable power and clocking
+    Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_RIT); //enable power and clocking
     
-    US_TICKER_TIMER->CTCR = 0x0; // timer mode
+    US_TICKER_TIMER->CTRL = RIT_CTRL_INT; // clear interrupt bit, disable timer
     uint32_t PCLK = SystemCoreClock / 4;
 
-    US_TICKER_TIMER->TCR = 0x2;  // reset
-
-    uint32_t prescale = PCLK / 1000000; // default to 1MHz (1 us ticks)
-    US_TICKER_TIMER->PR = prescale - 1;
-    US_TICKER_TIMER->TCR = 1; // enable = 1, reset = 0
+    // the RIT timer does not have a prescale register so it will always operate with PCLK ticks
+    // to get things into the "standard" range we use a postscale value. Note that this limits the
+    // range of event times that can be used to 85 seconds, but this is not typically an issue
+    // with how it is used.
+    postscale = PCLK / 1000000; // default to 1MHz (1 us ticks)
+    US_TICKER_TIMER->CTRL = RIT_CTRL_INT|RIT_CTRL_TEN; // clear interrupt bit, enable timer
 }
 
 uint32_t us_ticker_read() {
     if (!us_ticker_running)
         us_ticker_init();
 
-    return US_TICKER_TIMER->TC;
+    return US_TICKER_TIMER->COUNTER/postscale;
 }
 
 static inline void us_ticker_set_interrupt(unsigned int timestamp) {
-    // enable match interrupt, do it now to ensure we do not miss any matches
-    // there is a small chance that this and the code below may generate
-    // an extra interrupt, but that is harmless
-    US_TICKER_TIMER->MCR |= 1;
     // There is a chance that the timestamp may be in the past (or is about to be).
     // This can be caused by other higher priority interrupts delaying the actual
     // setting of the counters after the timestamp has been calculated/checked.
@@ -65,28 +63,33 @@ static inline void us_ticker_set_interrupt(unsigned int timestamp) {
     // timestamps to be moved to the "very near" future. Ensuring that they will
     // fire.
     while (true) {
+        // Clear any existing match
+        if (US_TICKER_TIMER->CTRL & RIT_CTRL_INT)
+            US_TICKER_TIMER->CTRL |= RIT_CTRL_INT;
         // set match value
-        US_TICKER_TIMER->MR[0] = timestamp;
+        US_TICKER_TIMER->COMPVAL = timestamp;
         // Is the time we have just set still in the future?
-        if ((int)(timestamp - US_TICKER_TIMER->TC) > 0) break;
+        if ((int)(timestamp - US_TICKER_TIMER->COUNTER) > 0) break;
         // Move timestamp forwards and try again
-        timestamp = US_TICKER_TIMER->TC + 1;
+        timestamp = US_TICKER_TIMER->COUNTER + postscale;
     }
+    NVIC_EnableIRQ(RITIMER_IRQn);
 }
 
 static inline void us_ticker_disable_interrupt(void) {
-    US_TICKER_TIMER->MCR &= ~1;
+    NVIC_DisableIRQ(RITIMER_IRQn);
 }
 
 static inline void us_ticker_clear_interrupt(void) {
-    US_TICKER_TIMER->IR = 1;
+    if (US_TICKER_TIMER->CTRL & RIT_CTRL_INT)
+        US_TICKER_TIMER->CTRL |= RIT_CTRL_INT;
 }
 
 static ticker_event_handler event_handler;
 static ticker_event_t *head = NULL;
 
 //void irq_handler(void) {
-void TIMER3_IRQHandler(void) {
+void RIT_IRQHandler(void) {
     us_ticker_clear_interrupt();
 
     /* Go through all the pending TimerEvents */
@@ -97,7 +100,7 @@ void TIMER3_IRQHandler(void) {
             return;
         }
 
-        if ((int)(head->timestamp - us_ticker_read()) <= 0) {
+        if ((int)(head->timestamp - US_TICKER_TIMER->COUNTER) <= 0) {
             // This event was in the past:
             //      point to the following one and execute its handler
             ticker_event_t *p = head;
@@ -118,13 +121,14 @@ void us_ticker_set_handler(ticker_event_handler handler) {
     event_handler = handler;
 
     //NVIC_SetVector(US_TICKER_TIMER_IRQn, (uint32_t)irq_handler);
-    NVIC_EnableIRQ(US_TICKER_TIMER_IRQn);
+    NVIC_EnableIRQ(RITIMER_IRQn);
 }
 
 void us_ticker_insert_event(ticker_event_t *obj, unsigned int timestamp, uint32_t id) {
     /* disable interrupts for the duration of the function */
     __disable_irq();
-
+    // Scale the uS time into our clock units
+    timestamp *= postscale;
     // initialise our data
     obj->timestamp = timestamp;
     obj->id = id;
