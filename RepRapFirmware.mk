c6b7e597c6fa4d1d7d1673b719bdd64c0769bdba
Andy Shaw <andy-git@gloomy-place.com>
2020-02-18 22:20:45 +0000

    Added new directory for 3.01-RC2

--- RepRapFirmware.mk
+++ RepRapFirmware.mk
@@ -6,7 +6,7 @@ RRF_SRC_DIRS = FilamentMonitors GCodes GCodes/GCodeBuffer Heating
 RRF_SRC_DIRS += Movement Movement/BedProbing Movement/Kinematics 
 RRF_SRC_DIRS += Storage Libraries/sha1
 RRF_SRC_DIRS += Heating/Sensors Fans ObjectModel Endstops Hardware Tools
-RRF_SRC_DIRS += Display Display/ST7920
+RRF_SRC_DIRS += Display Display/ST7920 GPIO
 #RRF_SRC_DIRS += Linux
 
 #LPC RRF Addons
